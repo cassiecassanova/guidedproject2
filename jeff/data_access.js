67a0da7b0d033abb3a873b7955da5cf95195dc40
josh_schilling <josh_schilling@aol.com>
2024-01-31 09:41:15 -0500

    Routes and everything from Step 1 to 3

---
+++ jeff/data_access.js
@@ -0,0 +1,112 @@
+const { MongoClient } = require("mongodb");
+var url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+const dbName = "starwars";
+const collectionFilms = "collectionFilms";
+const collectionCharacters = "collectionCharacters";
+const collectionPlanets = "collectionPlanets";
+const collectionFilmCharacters = "collectionFilmCharacters";
+const collectionFilmPlanets = "collectionFilmPlanets";
+// module.exports.findallcharacters = function(){
+//     return characters[]
+// }
+module.exports.call = async function call(operation, parameters, callback) {
+  // connect to the db server
+  await client.connect();
+  const parser = parseInt(parameters.id);
+  // set the database to use
+  const db = client.db(dbName);
+  // set the collection to use
+  //const collection = db.collection(collectionCharacters);
+
+  //Execute Operations
+  // available operations:
+  // ['initbooks'|'clearbooks'|'findallbooks'|'findbook'|'updatebook' ]
+  switch (operation.toLowerCase()) {
+    case "findallcharacters":
+      const characters = await db
+        .collection(collectionCharacters)
+        .find({})
+        .toArray();
+      callback({ characters: characters });
+      break;
+
+    case "findallplanets":
+      const planets = await db.collection(collectionPlanets).find({}).toArray();
+      callback({ planets: planets });
+      break;
+
+    case "findallfilms":
+      const films = await db.collection(collectionFilms).find({}).toArray();
+      callback({ films: films });
+      break;
+
+    case "findcharacter":
+      const character = await db
+        .collection(collectionCharacters)
+        .findOne({ id: parser });
+      callback({ character: character });
+      break;
+
+    case "findfilm":
+      const film = await db.collection(collectionFilms).findOne({
+        id: parser,
+      });
+      callback({ film: film });
+      break;
+
+    case "findplanet":
+      console.log(parser);
+      const planet = await db
+        .collection(collectionPlanets)
+        .findOne({ id: parser });
+      console.log(planet);
+      callback({ planet: planet });
+      break;
+
+    case "findfilmsforcharacters":
+      const filmschar = await db
+        .collection(collectionFilmCharacters)
+        .find({ character_id: parseInt(parameters.id) })
+        .toArray();
+      callback({ filmschar: filmschar });
+      break;
+
+    case "findcharactersforfilms":
+      const charfilms = await db
+        .collection(collectionFilmCharacters)
+        .find({ film_id: parseInt(parameters.id) })
+        .toArray();
+      callback({ charfilms: charfilms });
+
+    case "planetsforfilms":
+      const planfilms = await db
+        .collection(collectionFilmPlanets)
+        .find({ film_id: parseInt(parameters.id) })
+        .toArray();
+      callback({ planfilms: planfilms });
+
+    case "planetsforcharacters":
+    //   const answer =  db
+    //     .collection(collectionCharacters)
+    //     .find({ homeworld: parseInt(parameters.id) });
+      const plancharacters = await db
+        .collection(collectionCharacters)
+        .find({ plancharacters: parseInt(parameters.id) })
+        .toArray();
+      callback({ plancharacters: plancharacters });
+
+    case "filmsforplanets":
+      const filmplanets = await db
+        .collection(collectionFilmPlanets)
+        .find({ planet_id: parseInt(parameters.id) })
+        .toArray();
+      callback({ filmplanets: filmplanets });
+
+    default:
+      break;
+  }
+  console.log("call complete: " + operation);
+  client.close();
+  return "call complete";
+};
--- jeff/index.js
+++ jeff/index.js
@@ -1,14 +1,174 @@
-const express = require("express");
-
-
+const express = require('express');
+const dao = require("./data_access");
 const app = express();
 
 app.use(express.json());
 
 //GET REQUESTS
-app.get("", function(req, res) {
+//findplanet
+app.get("/planets/:id", (req, res) => {
+  dao.call("findplanet", { id: req.params.id }, (result) => {
+    if (result.planet !== undefined) {
+      res.send(result.planet);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+//findfilm
+app.get("/films/:id", (req, res) => {
+  dao.call("findfilm", {id: req.params.id}, (result) => {
+    if (result.film !== undefined) {
+      res.send(result.film);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+//set of films for character
+app.get("/characters/:id/films", (req, res) => {
+  dao.call("findfilmsforcharacters", { id: req.params.id }, (result) => {
+    if (result.filmschar !== undefined) {
+      res.send(result.filmschar);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+//set of characters for film
+app.get("/films/:id/characters", (req, res) => {
+  dao.call("findcharactersforfilms", { id: req.params.id }, (result) => {
+    if (result.charfilms !== undefined) {
+      res.send(result.charfilms);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
 
-})
+app.get("/films/:id/planets", (req, res) => {
+  dao.call("planetsforfilms", { id: req.params.id }, (result) => {
+    if (result.planfilms !== undefined) {
+      res.send(result.planfilms);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+
+app.get("/planets/:id/films", (req, res) => {
+  dao.call("filmsforplanets", { id: req.params.id }, (result) => {
+    if (result.filmplanets !== undefined) {
+      res.send(result.filmplanets);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+
+app.get("/planets/:id/characters", (req, res) => {
+  dao.call("planetsforcharacters", { id: req.params.id }, (result) => {
+    if (result.plancharacters !== undefined) {
+      res.send(result.plancharacters);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+//findcharacter
+app.get("/characters/:id", (req, res) => {
+  dao.call("findcharacter", {id: req.params.id}, (result) => {
+    if (result.character !== undefined) {
+      res.send(result.character);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+//findallcharacters
+app.get("/characters", (req, res) =>{
+    // let sw = dao.findallcharacters(req.params.id);
+    dao.call("findallcharacters", {}, (result) => {
+      if (result.characters !== undefined) {
+        res.send(result.characters);
+        console.log("In if statement");
+      } else {
+        res.statusCode = 404;
+        res.end();
+        console.log("In else statement");
+      }
+    });
+});
+
+app.get("/planets", (req, res) => {
+  dao.call("findallplanets", {}, (result) => {
+    if (result.planets !== undefined) {
+      res.send(result.planets);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+
+  app.get("/characters/:id", (req, res) => {
+  dao.call("findcharacter", {id: req.params.id}, (result) => {
+    if (result.character !== undefined) {
+      res.send(result.character);
+      console.log("In if statement");
+    } else {
+      res.statusCode = 404;
+      res.end();
+      console.log("In else statement");
+    }
+  });
+});
+
+  app.get("/films", (req, res) => {
+    // let sw = dao.findallcharacters(req.params.id);
+    dao.call("findallfilms", {}, (result) => {
+      if (result.films !== undefined) {
+        res.send(result.films);
+        console.log("In if statement");
+      } else {
+        res.statusCode = 404;
+        res.end();
+        console.log("In else statement");
+      }
+    });
+    // console.log(sw);
+    // res.send(sw);
+  });
+  // console.log(sw);
+  // res.send(sw);
+});
+
+
 
 //PUT REQUESTS
 
